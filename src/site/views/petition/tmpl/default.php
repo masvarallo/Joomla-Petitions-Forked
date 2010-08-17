07724c9a2b8689536d0846f63338f0cad0c25213
Kissaki <kissaki@gmx.de>
2010-08-17 15:54:25 +0200

    classes

--- src/site/views/petition/tmpl/default.php
+++ src/site/views/petition/tmpl/default.php
@@ -22,16 +22,18 @@
 
 <table width="100%" cellpadding="4" cellspacing="0" border="0" align="center" class="contentpane<?php echo $this->params->get( 'pageclass_sfx' ); ?>">
 	<?php if ( @$this->category->image || @$this->category->description ) : ?>
-		<tr>
+		<tr class="petition_info">
 			<td valign="top" class="contentdescription<?php echo $this->params->get( 'pageclass_sfx' ); ?>">
 			<?php
-				if ( isset($this->category->image) ) :  echo $this->category->image; endif;
+				if ( isset($this->category->image) ) :
+					echo $this->category->image;
+				endif;
 				echo $this->category->description;
 			?>
 			</td>
 		</tr>
 	<?php endif; ?>
-	<tr>
+	<tr class="petition_signees">
 		<td width="60%" colspan="2">
 			<?php if (!$this->params->get('signataires_masque', 0)) : ?>
 				<?php echo $this->loadTemplate('items'); ?>
