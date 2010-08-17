8c10dda08d74dd8187284db611536e31ce0acaa8
Kissaki <kissaki@gmx.de>
2010-08-17 16:36:38 +0200

    refactor

--- src/site/views/petition/tmpl/default.php
+++ src/site/views/petition/tmpl/default.php
@@ -15,24 +15,24 @@
 	<div class="componentheading<?php echo $this->params->get( 'pageclass_sfx' ); ?>">
 		<?php echo $this->category->title; ?>
 	</div>
-	<span class="small">
-			(<?php echo $this->total;?> <?php JText::_('petitions number of signees')?>)
-	</span>
+	<div class="small com_petitions_signeecount">
+			(<?php echo $this->total;?> <?php echo JText::_('petitions number of signees')?>)
+	</div>
 <?php endif; ?>
 
+<?php if ( @$this->category->image || @$this->category->description ) : ?>
+	<div class="petition_info contentdescription<?php echo $this->params->get( 'pageclass_sfx' ); ?>">
+		<?php
+			if ( isset($this->category->image) ) :
+				echo $this->category->image;
+			endif;
+		?>
+		<div class="petition_info_description">
+			<?php echo $this->category->description; ?>
+		</div>
+	</div>
+<?php endif; ?>
 <table width="100%" cellpadding="4" cellspacing="0" border="0" align="center" class="contentpane<?php echo $this->params->get( 'pageclass_sfx' ); ?>">
-	<?php if ( @$this->category->image || @$this->category->description ) : ?>
-		<tr class="petition_info">
-			<td valign="top" class="contentdescription<?php echo $this->params->get( 'pageclass_sfx' ); ?>">
-			<?php
-				if ( isset($this->category->image) ) :
-					echo $this->category->image;
-				endif;
-				echo $this->category->description;
-			?>
-			</td>
-		</tr>
-	<?php endif; ?>
 	<tr class="petition_signees">
 		<td width="60%" colspan="2">
 			<?php if (!$this->params->get('signataires_masque', 0)) : ?>
